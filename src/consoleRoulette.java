1c361062224318ac5375a7fe2269a63fc70a6c4f
makhado <makhadolucky@gmail.com>
2021-02-17 16:27:46 +0200

    added inputs

--- src/consoleRoulette.java
+++ src/consoleRoulette.java
@@ -7,18 +7,54 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class consoleRoulette {
 
-    public static void main(String [] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         File inputFile = new File("/home/makahdo/Documents/Input_file.txt");
+
         ArrayList<String> nameList = (ArrayList<String>) Files.readAllLines(inputFile.toPath(), Charset.defaultCharset());
 
-        for (String s : nameList) {
+        boolean repeat;
+        do {
+            repeat = true;
+            for (String s : nameList) {
+
+                String betInput = null;
+                int betNumber = 0;
+                double amount = 0.0;
+//                int randomBall = 0;
+//                double amountWon;
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println(s);
+                while (!scanner.hasNextInt()) {
+                    betInput = scanner.next();
+                    if (!(betInput.equalsIgnoreCase("EVEN") || betInput.equalsIgnoreCase("ODD"))) {
+                        betNumber = Integer.getInteger(betInput);
+                    } else break;
+
+                }
+
+                while (scanner.hasNextInt()) {
+                    betNumber = scanner.nextInt();
+                    if (betNumber > 36 || betNumber < 1) {
+                        System.out.println("Bet number must be between 36 and 1, EVEN or ODD");
+                    } else {
+                        break;
+                    }
+                }
+
+                amount = scanner.nextDouble();
 
 
-        }
+            }
+
+
+        } while (repeat);
 
     }
 
